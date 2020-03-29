cf8d6116524297cba93312baab8b32c71cb95d08
Ryan Hunter <>
2020-03-29 13:58:17 -0700

    basic evaluation function, weighted pieces

---
+++ evaluation.py
@@ -0,0 +1,31 @@
+import chess
+
+class EvalParams:
+   
+   def __init__(self):
+      self.pawn_w = 10 
+      self.knight_w = 30
+      self.bishop_w = 30
+      self.rook_w = 50
+      self.queen_w = 90
+      self.king_w = 900
+      
+
+def evaluateBoard(board, params):
+   wpval = params.pawn_w * len(board.pieces(chess.PAWN, chess.WHITE))
+   bpval = -1 * params.pawn_w * len(board.pieces(chess.PAWN, chess.BLACK))
+
+   wkval = params.knight_w * len(board.pieces(chess.KNIGHT, chess.WHITE))
+   bkval = -1 * params.knight_w * len(board.pieces(chess.KNIGHT, chess.BLACK))
+
+   wbval = params.bishop_w * len(board.pieces(chess.BISHOP, chess.WHITE))
+   bbval = -1 * params.bishop_w * len(board.pieces(chess.BISHOP, chess.BLACK))
+
+   wrval = params.rook_w * len(board.pieces(chess.ROOK, chess.WHITE))
+   brval = -1 * params.rook_w * len(board.pieces(chess.ROOK, chess.BLACK))
+
+   wqval = params.queen_w * len(board.pieces(chess.QUEEN, chess.WHITE))
+   bqval = -1 * params.queen_w * len(board.pieces(chess.QUEEN, chess.BLACK))
+   
+   return wpval + bpval + wkval + bkval + wbval + bbval + wrval + brval + wqval + bqval
+
--- main.py
+++ main.py
@@ -1,23 +1,80 @@
 import chess
 import random
-import chess.svg
+import evaluation
 
+evalParams = evaluation.EvalParams()
 
+# alpha beta functions based heavily on psuedo-code from here:
+# https://www.chessprogramming.org/Alpha-Beta
 
+def alphaBetaMax(board, alpha, beta, depthLeft):
+   if depthLeft == 0:
+      return evaluation.evaluateBoard(board, evalParams)
 
+   for move in board.legal_moves:
+      board.push(move)
+      score = alphaBetaMin(board, alpha, beta, depthLeft - 1)
+      board.pop()
+      if score >= beta:
+         return beta
+      if score > alpha:
+         alpha = score
+
+   return alpha
+
+def alphaBetaMin(board, alpha, beta, depthLeft):
+   if depthLeft == 0:
+      return evaluation.evaluateBoard(board, evalParams)
+
+   for move in board.legal_moves:
+      board.push(move)
+      score = alphaBetaMax(board, alpha, beta, depthLeft - 1)
+      board.pop()
+      if score <= alpha:
+         return alpha
+      if score < beta:
+         beta = score
+
+   return beta
 
 def get_move(board):
    legal_moves = [move for move in board.legal_moves]
+   best_move = legal_moves[0]
 
-   return legal_moves[random.randint(0, len(legal_moves) - 1)]
+   best_score = -9999
+   beta = 9999
+   alpha = -9999
+   depth = 1
+
+   for move_to_eval in legal_moves:
+      board.push(move_to_eval)
+      move_score = alphaBetaMax(board, alpha, beta, 1)
+      if move_score > best_score:
+         best_score = move_score
+         best_move = move_to_eval
+      board.pop()
+   
+   return best_move
 
 def make_move(board):
    board.push(get_move(board))
 
+def make_random_move(board):
+   legal_moves = [move for move in board.legal_moves]
+   board.push(legal_moves[random.randint(0, len(legal_moves) - 1)])
+
 if __name__ == "__main__":
    board = chess.Board()
    while(not board.is_game_over()):
-      make_move(board) 
+      if(board.turn):
+         make_move(board) 
+      else:
+         make_random_move(board)
       print(board)
       print("------------------------------------")
+
    print(board.result())
+   print('Is stalemate: {0}'.format(board.is_stalemate()))
+   print('Is insufficient: {0}'.format(board.is_insufficient_material()))
+   print('Is fivefold: {0}'.format(board.is_fivefold_repetition()))
+
